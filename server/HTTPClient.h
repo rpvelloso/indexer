d42830376563e2f5f1307e5094bf51a70380d306
rvelloso <rvelloso@DTI351996.tjsc.ad>
2018-03-27 17:12:58 -0300

    correct GPL message

--- server/HTTPClient.h
+++ server/HTTPClient.h
@@ -1,16 +1,16 @@
 /*
     Copyright 2018 Roberto Panerai Velloso.
-    This file is part of libjukebox.
-    libjukebox is free software: you can redistribute it and/or modify
+    This file is part of indexer.
+    Indexer is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    libjukebox is distributed in the hope that it will be useful,
+    Indexer is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with libjukebox.  If not, see <http://www.gnu.org/licenses/>.
+    along with Indexer.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef HTTPCLIENT_H_
